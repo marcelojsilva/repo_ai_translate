f10953c99e10dbc60d52ff20a5fa9e836546851c
marcelojsilva <marcelojosedasilva@gmail.com>
2024-02-12 20:40:25 -0300

    Translate programming comments and non-markdown files to a different language.

--- repo-ai-trans.py
+++ repo-ai-trans.py
@@ -11,15 +11,21 @@ def translate_text(text, prompt):
     chat_completion = client.chat.completions.create(
         messages=[
             {
+                "role": "system",
+                "content": f"{prompt}",
+            },
+            {
                 "role": "user",
-                "content": f"{text}\n\n{prompt}:",
+                "content": f"{text}",
             }
         ],
         model="gpt-3.5-turbo-0125",
         max_tokens=4096,
         temperature=0,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
     )
-    
     return chat_completion.choices[0].message.content.strip()
 
 def replace_links(text, target_language, original_language):
@@ -54,7 +60,7 @@ def replace_links(text, target_language, original_language):
 def translate_file(original_file_path, target_file_path, target_language):
     with open(original_file_path, 'r', encoding='utf-8') as f:
         text = f.read()
-    prompt = f'Translate the above text to {target_language}'
+    prompt = f'Translate text to {target_language}'
     translated_text = translate_text(text, prompt)
     translated_text = replace_links(translated_text, target_language, original_language)
     os.makedirs(os.path.dirname(target_file_path), exist_ok=True)
@@ -62,17 +68,18 @@ def translate_file(original_file_path, target_file_path, target_language):
         f.write(translated_text)
         f.write("\n\n<!-- This file was translated using AI by repo_ai_translate. For more information, visit https://github.com/marcelojsilva/repo_ai_translate -->")
 
-def copy_non_md_files(original_path, target_path):
+def copy_non_md_programming_files(original_path, target_path):
     files = glob.glob(os.path.join(original_path, '**/*'), recursive=True)
+    files = [f for f in files if not f.startswith(os.path.join(original_path, 'Languages'))]
     for file in files:
-        if not file.endswith('.md') and not os.path.isdir(file):
+        if not file.endswith('.md') and not os.path.isdir(file) and not is_programming_file(file):
             # Get the relative path difference
             rel_path = os.path.relpath(file, original_path)
             # Create the same directory structure in the target path
             target_file_path = os.path.join(target_path, rel_path)
             if os.path.exists(target_file_path):
                 continue
-            os.makedirs(os.path.dirname(target_file_path), exist_ok=True)
+            os.makedirs(os.path.dirname(file), exist_ok=True)
             shutil.copy(file, target_file_path)
 
 def is_programming_file(filename):
@@ -96,40 +103,47 @@ def add_translation_link(target_file_path, target_language):
                 break
         else:
             # If no link to a translated readme is found, generate one using OpenAI
-            link_text = translate_text(f"Read this document in {target_language}:", target_language)
+            link_text = translate_text(f"Read this document in {target_language}:", f'Translate text to {target_language}')
             new_line = f"[{link_text}](readme_{target_language}.md)\n"
             lines.insert(0, new_line)
         f.seek(0)
         f.writelines(lines)
         f.truncate()
 
-def translate_comments(file_path, original_language, target_language):
-    with open(file_path, 'r', encoding='utf-8') as f:
+def translate_comments(original_file, target_file, original_language, target_language):
+    with open(original_file, 'r', encoding='utf-8') as f:
         lines = f.readlines()
 
     # Regular expression to match comments in the original language
     comment_regex = re.compile(r'//.*|/\*.*?\*/|<!--.*?-->|#.*|\'\'\'.*?\'\'\'|\"\"\".*?\"\"\"', re.DOTALL)
-
     i = 0
     while i < len(lines):
+        comment = ""
+        end_line = i
+        # If the comment is a multiline comment, get all lines
+        if lines[i].lstrip().startswith(('/*', '"""', "'''", '<!--')):
+            comment += lines[end_line]
+            while not comment.rstrip().endswith(('*/', '"""', "'''", '-->')):
+                end_line += 1
+                comment += lines[end_line]
         match = comment_regex.search(lines[i])
-        if match:
+        if match and 'SPDX-License-Identifier:' not in lines[i]:
             comment = match.group()
-            end_line = i
-            # If the comment is a multiline comment, get all lines
-            if comment.startswith('/*') or comment.startswith('"""') or comment.startswith("'''") or comment.startswith('<!--'):
-                while not comment.endswith('*/') and not comment.endswith('"""') and not comment.endswith("'''") and not comment.endswith('-->'):
-                    end_line += 1
-                    comment += lines[end_line]
-            prompt = f'\n\nTranslate the above if the text have some part in {original_language}, if yes, translate the entire text to {target_language}, otherwise, leave it as is (DO NOT INCLUDE ANYTHING BUT THE TRANSLATED TEXT, EXCEPT COMMENTS CHARACTERS):'
+        if comment:
+            prompt = f'Translate the text if the text has any part in \'{original_language}\', if so, translate the entire text to \'{target_language}\', else keep the original text. (DO NOT INCLUDE ANYTHING OTHER THAN THE TRANSLATED TEXT. LEAVE COMMENTS CHARACTERS, ESCAPE CHARACTERS AND SPACES. DO NOT INCLUDE ANY PART OF THE ROLE SYSTEM CONTENT).'
             translated_comment = translate_text(comment, prompt)
-            for j in range(i, end_line + 1):
-                lines[j] = lines[j].replace(comment.lstrip(), translated_comment.lstrip())
+            translated_comment_lines = translated_comment.split('\n')
+            if not translated_comment.endswith('\n'):
+                translated_comment += '\n'
+            for j in range(i, len(translated_comment_lines) + i):
+                lines[j] = ' ' * (len(lines[j]) - len(lines[j].lstrip())) + translated_comment_lines[j - i].lstrip() + '\n'
             i = end_line
         i += 1
 
-    with open(file_path, 'w', encoding='utf-8') as f:
+    os.makedirs(os.path.dirname(target_file), exist_ok=True)
+    with open(target_file, 'w', encoding='utf-8') as f:
         f.writelines(lines)
+
 
 if __name__ == '__main__':
     original_language = sys.argv[1]
@@ -145,8 +159,12 @@ if __name__ == '__main__':
             continue
         translate_file(md_file, target_file_path, target_language)
 
-    copy_non_md_files(original_path, target_path)
+    copy_non_md_programming_files(original_path, target_path)
 
-    programming_files = [file for file in glob.glob(os.path.join(target_path, '**/*'), recursive=True) if is_programming_file(file)]
+    programming_files = [file for file in glob.glob(os.path.join(original_path, '**/*'), recursive=True) if is_programming_file(file)]
     for programming_file in programming_files:
-        translate_comments(programming_file, original_language, target_language)
+        relative_path = os.path.relpath(programming_file, original_path)
+        target_file_path = os.path.join(target_path, relative_path)
+        if os.path.exists(target_file_path):
+            continue
+        translate_comments(programming_file, target_file_path, original_language, target_language)
